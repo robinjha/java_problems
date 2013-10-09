dc9bdffd322fe714a199c4acc26f2101f2cc2c18
Robin Jha <robinjha@gmail.com>
2013-10-09 15:11:50 -0400

    LinkedList Implementation

---
+++ src/com/java/collection/LinkedList.java
@@ -0,0 +1,21 @@
+package com.java.collection;
+
+import java.util.Iterator;
+
+public class LinkedList<T> implements Iterable<T> {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Iterator<T> iterator() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
