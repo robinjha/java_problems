ff4ef82bb31dacfffde1d644e9c5da8f31e60ea8
robinjha <robinjha@gmail.com>
2013-10-29 21:00:23 -0400

    adding values in LL iterative solution

--- src/com/java/collection/AddTwoNumbersInLL.java
+++ src/com/java/collection/AddTwoNumbersInLL.java
@@ -2,22 +2,42 @@ package com.java.collection;
 
 public class AddTwoNumbersInLL {
 	
+	/**
+	 * this solution works if the LL are the same length
+	 * @param n1
+	 * @param n2
+	 * @return
+	 */
 	public Node addTwoValuesinLL(Node n1, Node n2){
 		if(n1 == null)return n2;
 		if(n2 == null)return n1;
 		Node newHead = null;
-		int carry = 0, val = 0;
+		int carry = 0, val = 0, nodeVal = 0;
 		
-		while(n1 != null && n2 != null && carry != 0){
+		while(n1 != null && n2 != null){
 			val = n1.getData() + n2.getData()+carry;
-				carry = val % 10;
+			if(val > 10)
+				carry = 1;
+			else
+				carry = 0;
 			
-			val = val - carry;
-			newHead = new Node(val, newHead);
+			nodeVal = val % 10;
+			newHead = new Node(nodeVal, newHead);
 			n1 = n1.getNext();
 			n2 = n2.getNext();
 		}
+		if(carry != 0){
+			newHead = new Node(carry, newHead);
+		}
 		return newHead;
+	}
+	
+	/**
+	 * recursive solution
+	 * @param args
+	 */
+	public Node addTwoValuesinLLRecursively(Node n1, Node n2){
+		return null;
 	}
 
 	public static void main(String[] args) {
@@ -30,7 +50,7 @@ public class AddTwoNumbersInLL {
 		Node list2 = new Node(5);
 		Node d = new Node(9);
 		list2.setNext(d);
-		Node e = new Node(2);
+		Node e = new Node(8);
 		d.setNext(e);
 		
 		AddTwoNumbersInLL blah = new AddTwoNumbersInLL();
